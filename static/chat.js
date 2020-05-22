bb23054ae1c74d201ddf62ff22833c095993a894
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-05-22 16:32:27 +0100

    indentation of messages fixed

--- static/chat.js
+++ static/chat.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     localStorage.setItem('room', room)
 
+    // format existing posts
+    indent();
+
     // set button function
     document.querySelector('#send').onclick = function () {
 
@@ -21,31 +24,65 @@ document.addEventListener("DOMContentLoaded", function () {
             'user': user,
             'time': time
         });
+
     };
 
     socket.on('sendMsg', function (data) {
 
-        add(data);
-        clearBox();
+        const current = localStorage.getItem('room')
+
+        if (data['room'] == current) {
+            add(data);
+        }
+
+        clearBox(data);
     });
 });
 
 function add(data) {
 
-    // Create msg template
-    const template = Handlebars.compile(document.querySelector('#postMsg').innerHTML);
+    if (data['user'] == localStorage.getItem('user')) {
+        
+        // Create msg template
+        const template = Handlebars.compile(document.querySelector('#postMsg1').innerHTML);
 
-    // Add msg to DOM.
-    const content = template({
-        'msg': data.msg,
-        'user': data.user,
-        'time': data.time
-    });
+        // Add msg to DOM.
+        const content = template({
+            'msg': data.msg,
+            'user': data.user,
+            'time': data.time
+        });
 
-    document.querySelector('#msgBox').innerHTML += content;
+        document.querySelector('#msgBox').innerHTML += content;
+    
+    } else {
+        // Create msg template
+        const template = Handlebars.compile(document.querySelector('#postMsg2').innerHTML);
+
+        // Add msg to DOM.
+        const content = template({
+            'msg': data.msg,
+            'user': data.user,
+            'time': data.time
+        });
+
+        document.querySelector('#msgBox').innerHTML += content;
+    };
 };
 
-function clearBox() {
+function indent() {
 
-    document.querySelector('#txtBox').value = '';
+    const user = localStorage.getItem('user');
+
+    document.querySelectorAll(`#${user}`).forEach(function(div) {
+
+        div.className = 'card w-75 ml-auto mt-2 mb-2';
+    });
+}
+
+function clearBox(data) {
+
+    if(data['user'] == localStorage.getItem('user')) {
+        document.querySelector('#txtBox').value = '';
+    }
 };
